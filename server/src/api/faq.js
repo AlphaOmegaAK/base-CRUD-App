e08dde3b36dc256f6fb4890206dd81a1fdf5bfb7
Artur <alphaunionxt@gmail.com>
2020-10-01 09:46:31 -0400

    Base Set-up for CRUD routes

--- server/src/api/faq.js
+++ server/src/api/faq.js
@@ -2,8 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
+
+//  Read All
 router.get('/', (req, res, next) => {
 
-})
+});
+//  Read One
+router.get('/:id', (req, res, next) => {
 
+});
+//  Create
+router.post('/', (req, res, next) => {
+
+});
+//  Update
+router.put('/:id', (req, res, next) => {
+
+});
+//  Delete
+router.delete('/:id', (req, res, next) => {
+
+});
 module.exports = router;
