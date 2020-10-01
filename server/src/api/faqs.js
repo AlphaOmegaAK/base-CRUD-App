6cc05fc33f6c9c96fe742f7f397406279c520c74
Artur <alphaunionxt@gmail.com>
2020-10-01 10:08:32 -0400

    using monk for database access and @hapi/joi to validate the incoming data"

--- server/src/api/faqs.js
+++ server/src/api/faqs.js
@@ -1,26 +1,41 @@
 const express = require('express');
+const monk = require('monk');
+
+
+const db = monk(process.env.MONGODB_URI);
+const faqs = db.get('faqs');
 
 const router = express.Router();
 
 
 //  Read All
 router.get('/', (req, res, next) => {
-
+    res.json({
+        msg: 'Show All',
+    });
 });
 //  Read One
 router.get('/:id', (req, res, next) => {
-
+    res.json({
+        msg: 'Show One',
+    });
 });
 //  Create
 router.post('/', (req, res, next) => {
-
+    res.json({
+        msg: 'Create',
+    });
 });
 //  Update
 router.put('/:id', (req, res, next) => {
-
+    res.json({
+        msg: 'Update',
+    });
 });
 //  Delete
 router.delete('/:id', (req, res, next) => {
-
+    res.json({
+        msg: 'Delete',
+    });
 });
 module.exports = router;
--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
 const emojis = require('./emojis');
+const faqs = require('./faqs');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏'
+    message: 'API - 👋🌎🌍🌏 helllo'
   });
 });
 
 router.use('/emojis', emojis);
+router.use('/faqs', faqs);
 
 module.exports = router;
