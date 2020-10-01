dcc5d340800b69ddf2fd2296ca050676350a409d
Artur <alphaunionxt@gmail.com>
2020-10-01 10:25:21 -0400

    Create Route Operational after a few tweaks

--- server/src/api/faqs.js
+++ server/src/api/faqs.js
@@ -6,14 +6,12 @@ const db = monk(process.env.MONGODB_URI);
 const faqs = db.get('faqs');
 
 const schema = Joi.object({
-    question: Joi.string().trim().required(),
-    answer: Joi.object().trim().required(),
+    question: Joi.string().required(),
+    answer: Joi.string().required(),
     video_url: Joi.string().uri(),
 });
 
-
 const router = express.Router();
-
 
 //  Read All
 router.get('/', async (req, res, next) => {
@@ -25,31 +23,39 @@ router.get('/', async (req, res, next) => {
         next(error);
     }
 });
+
 //  Read One
 router.get('/:id', (req, res, next) => {
     res.json({
         msg: 'Show One',
     });
 });
+
 //  Create
 router.post('/', async (req, res, next) => {
     try {
 
+        console.log(req.body);
+        const value = await schema.validateAsync(req.body);
+        res.json(value)
 
     } catch (error) {
-
+        next(error);
     }
 });
+
 //  Update
 router.put('/:id', (req, res, next) => {
     res.json({
         msg: 'Update',
     });
 });
+
 //  Delete
 router.delete('/:id', (req, res, next) => {
     res.json({
         msg: 'Delete',
     });
 });
+
 module.exports = router;
